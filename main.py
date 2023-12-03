df160d4896fa1923efb02e4d179f0932d8b33216
ankithh57 <152783130+ankithh57@users.noreply.github.com>
2023-12-03 17:39:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 
 def count_batteries_by_health(present_capacities):
+  healthy = 0
+  exchange = 0
+  failed = 0
+  for i in present_capacities:
+    SoH= 100*i//120
+    if SoH >= 80:
+      healthy+=1
+    elif SoH < 80 and SoH >= 62:
+      exchange+=1
+    else:
+      failed+=1
   return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+    "healthy":healthy,
+    "exchange":exchange,
+    "failed": failed
+    
   }
 
 
